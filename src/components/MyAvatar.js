40ac962feac4e7a0cfb0f2bbca9c8319b28d4f74
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-12-16 20:51:04 -0300

    Avatar responsive in AboutMeCard

--- src/components/MyAvatar.js
+++ src/components/MyAvatar.js
@@ -10,14 +10,24 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
     },
   },
-  small: {
-    width: theme.spacing(3),
-    height: theme.spacing(3),
-  },
-  large: {
-    width: theme.spacing(37),
-    height: theme.spacing(37),
-  },
+  avatar: {
+    [theme.breakpoints.down("xl")]: {
+      width: theme.spacing(39),
+      height: theme.spacing(39),
+    },
+    [theme.breakpoints.down("lg")]: {
+      width: theme.spacing(35),
+      height: theme.spacing(35),
+    },
+    [theme.breakpoints.down("md")]: {
+      width: theme.spacing(30),
+      height: theme.spacing(30),
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: theme.spacing(20),
+      height: theme.spacing(20),
+    },
+  }
 }));
 
 export default function ImageAvatars() {
@@ -25,7 +35,7 @@ export default function ImageAvatars() {
 
   return (
     <div className={classes.root}>
-      <Avatar alt="Nicolas Sawczuk" src={nico} className={classes.large} />
+      <Avatar alt="Nicolas Sawczuk" src={nico} className={classes.avatar} />
     </div>
   );
 }

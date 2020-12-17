dc242f46a094a75b228b96b667d9bf13926aea78
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-12-16 22:25:11 -0300

    Grow transition in about me container

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -12,6 +12,7 @@ import useTheme from '../hooks/useTheme';
 import AboutMeCard from '../components/AboutMeCard';
 import TimelineFormation from '../components/TimelineFormation';
 import ListOfSkills from '../components/ListOfSkills';
+import Grow from '@material-ui/core/Grow';
 
 
 
@@ -21,29 +22,33 @@ export default function AboutMe() {
 
 
 
-    return (
-      <ThemeProvider theme={darkTheme}>
-        <div className={classes.root}>
-          <CssBaseline />
-          <Sidebar title="Sobre mi" changeTheme={changeTheme} darkState={darkState} />
-          <main className={classes.content}>
-            <div className={classes.appBarSpacer} />
-            <Container maxWidth="lg" className={classes.container} spacing={2}>
-              <Grid item>
-                <AboutMeCard />
-              </Grid>
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <Sidebar title="Sobre mi" changeTheme={changeTheme} darkState={darkState} />
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container} spacing={2}>
+            <Grid item>
+              <AboutMeCard />
+            </Grid>
 
+            <Grow in={true}>
               <Grid item className={classes.gridItem}>
-                <Typography variant="h4"  color="secondary">Formación académica</Typography>
+                <Typography variant="h4" color="secondary">Formación académica</Typography>
                 <TimelineFormation />
               </Grid>
+            </Grow>
+            <Grow in={true}>
               <Grid container spacing={3} className={classes.gridItem}>
-              <Typography variant="h4"  color="secondary">Tecnologías</Typography>
+                <Typography variant="h4" color="secondary">Tecnologías</Typography>
                 <ListOfSkills />
               </Grid>
-            </Container>
-          </main>
-        </div>
-      </ThemeProvider>
-    );
+            </Grow>
+          </Container>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
 }

784561ca36e7c358e79605f110e7eb9007d16693
Nicolas Sawczuk <nico.290698@hotmail.com>
2020-12-29 21:07:40 -0300

    Changes in FirebaseServices

--- src/services/FirebaseServices.js
+++ src/services/FirebaseServices.js
@@ -1,19 +1,4 @@
-import firebase from 'firebase'
-
-var firebaseConfig = {
-    apiKey: "AIzaSyDUqxIpZchAJUDyR4lntWS8bh2EqfR7HQM",
-    authDomain: "nicosawczuk-site.firebaseapp.com",
-    databaseURL: "https://nicosawczuk-site.firebaseio.com",
-    projectId: "nicosawczuk-site",
-    storageBucket: "nicosawczuk-site.appspot.com",
-    messagingSenderId: "292706005601",
-    appId: "1:292706005601:web:c3fc714930f584a512f020"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-
-const database = firebase.database().ref("/projects");
+import {database} from './firebaseConfig'
 
 
 
